cd4fbf1e208de53dda32ef097f050b3af5843731
wjdgns7712 <anthony9307@naver.com>
2019-12-15 16:23:00 +0900

    newone

--- snake_game/main.c
+++ snake_game/main.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<curses.h>
+#include<pthread.h>
 
-// define
-enum dir { up = 'w', down = 's', left = 'a', right = 'd'};
+// define #
+enum dir { up = 'w', down = 's', left = 'a', right = 'd' };
 enum type { wall = -1, none = 0, body = 1, food = 2 };
 typedef struct { int type, lock; } tile;
 typedef struct { int x, y; } pos;
 
 // declare # func
 tile** init_setting(int size, pos start_point);
-void play();
 void change_head(int next_dir);
-void move();
+void move_head();
 void print();
 
 // declare # value
@@ -25,18 +26,18 @@ int size;
 int main(void)
 {
 	// set size
-	while(1)
+	while (1)
 	{
 		printf("Set field size greater than 10. ( If type 0, set default 51 )\nSize : ");
 		scanf("%d", &size);
 		if (size == 0)
 			size = 51;
-		if (size > 10 )
+		if (size > 10)
 			break;
 		if (size <= 10)
 			printf("Set too small size, set again greater than 10.\n");
 	}
-	
+
 	// start initialization
 	length = 3;
 	next_dir = left;
@@ -44,8 +45,19 @@ int main(void)
 	field = init_setting(size, head);
 
 	// start game
-	play();
-	
+	initscr();
+	print();
+	while (1)
+	{
+		scanf("%d", &next_dir);
+		change_head(next_dir);
+		if (field[head.y][head.x].lock != 0) // end condition
+			break;
+		move_head();
+		print();
+		refresh();
+	}
+	endwin();
 
 	return 0;
 }
@@ -65,46 +77,32 @@ tile** init_setting(int size, pos start_point)
 	}
 
 	// make basic map
-	for(int i=0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		tmp[0][i].type = wall;
 		tmp[0][i].lock = -1;
 	}
-	for (int i = 1; i < size-1; i++)
+	for (int i = 1; i < size - 1; i++)
 	{
 		tmp[i][0].type = wall;
 		tmp[i][0].lock = -1;
-		tmp[i][size-1].type = wall;
+		tmp[i][size - 1].type = wall;
 		tmp[i][size - 1].lock = -1;
 	}
 	for (int i = 0; i < size; i++)
 	{
-		tmp[size-1][i].type = wall;
+		tmp[size - 1][i].type = wall;
 		tmp[size - 1][i].lock = -1;
 	}
 
 	// get ready
 	for (int i = length; i > 0; i--)
 	{
-		tmp[start_point.y][start_point.x - length + i].lock = i;
-		tmp[start_point.y][start_point.x - length + i].type = body;
+		tmp[start_point.y][start_point.x + length - i].lock = i;
+		tmp[start_point.y][start_point.x + length - i].type = body;
 	}
 
 	return tmp;
-}
-
-void play()
-{
-	while (1)
-	{
-//		sleep(1);
-		scanf("%d", &next_dir);
-		change_head(next_dir);
-		if (field[head.y][head.x].lock != 0)
-			break;
-		move();
-		print();
-	}
 }
 
 void change_head(int dir)
@@ -112,22 +110,22 @@ void change_head(int dir)
 	// change head pointer
 	switch (dir)
 	{
-		case up:
-			head.y = head.y - 1;
-			break;
-		case down:
-			head.y = head.y +1;
-			break;
-		case left:
-			head.x = head.x - 1;
-			break;
-		case right:
-			head.x = head.x + 1;
-			break;
+	case up:
+		head.y = head.y - 1;
+		break;
+	case down:
+		head.y = head.y + 1;
+		break;
+	case left:
+		head.x = head.x - 1;
+		break;
+	case right:
+		head.x = head.x + 1;
+		break;
 	}
 }
 
-void move()
+void move_head()
 {
 	// change field status
 	field[head.y][head.x].lock = length;
@@ -139,70 +137,31 @@ void move()
 
 void print()
 {
-	for(int i = 0; i < size; i++)
+	move(0, 0);
+	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
 		{
 			switch (field[i][j].type)
 			{
 			case wall:
-//				printf("бс");
-				printf("*");
+				//				printf("бс");
+//				addstr("бс");
+				addstr("*");
 				break;
 			case body:
-//				printf("б▄");
-				printf("@");
+				//				printf("б▄");
+				addstr("@");
 				break;
 			case none:
-//				printf("бр");
-				printf(" ");
+				//				printf("бр");
+				addstr(" ");
 				break;
 			case food:
-//				printf("б┌");
+				//				printf("б┌");
 				break;
 			}
 		}
-		printf("\n");
+		move(i, 0);
 	}
 }
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-
-int size;
-wchar_t** box;
-struct warm
-{
-	char a[50];
-};
-
-void init(int size);
-
-int main(void)
-{
-	setlocale(LC_ALL, "");
-	
-
-	init(size);
-	for (int i = 0; i < size; i++)
-		printf("%ls\n", box[i]);
-	return 0;
-}
-void init(int size)
-{
-	box = (wchar_t**)malloc(sizeof(wchar_t*) * size);
-	for (int i = 0; i < size; i++)
-	{
-		box[i] = (wchar_t*)malloc(sizeof(wchar_t) * size);
-		for (int j = 0; j < size; j++)
-		{
-			box[i][j] = L'бр';
-			if (i == 0 || i == size - 1)
-				box[i][j] = L'бс';
-			if (j == 0 || j == size - 1)
-				box[i][j] = L'бс';
-		}
-	}
-}
-*/
